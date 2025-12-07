b8681703cc1a2c88bbb8af867d4d412f9f1df7a8
root <root@DESKTOP-9C87IIF.localdomain>
2025-12-07 19:24:59 +0100

    minitalk finito

---
+++ srcs/libft/ft_realloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldesboui <ldesboui@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 18:43:28 by ldesboui          #+#    #+#             */
+/*   Updated: 2025/12/07 18:43:28 by ldesboui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void *ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+    void *new_ptr;
+
+    if (new_size == 0)
+    {
+        free(ptr);
+        return (NULL);
+    }
+    new_ptr = ft_calloc(new_size, 1);
+    if (!new_ptr)
+    {
+        free(ptr);
+        return (NULL);
+    }
+    ft_memcpy(new_ptr, ptr, old_size);
+    free(ptr);
+    return (new_ptr);
+}
--- srcs/server/main.c
+++ srcs/server/main.c
@@ -12,61 +12,41 @@
 
 #include "../../includes/minitalk.h"
 
-char ***strs;
+char *str;
 
-static void printstrings()
+static void create_char(char *c)
 {
-    int i;
+    size_t len;
 
-    i = 0;
-    while (strs[0][i])
-    {
-        ft_printf("%s", strs[0][i]);
-        free(strs[0][i]);
-        ++i;
-    }
-    free(strs[0]);
-    free(strs);
-}
-static void freeall(char ***strs)
-{
-    int i;
-
-    i = 0;
-    if (strs)
-    {
-        while (strs[0][i])
+    len = ft_strlen(str);
+    if (*c == '\0')
         {
-            free(strs[0][i]);
-            ++i;
+            ft_printf("%s\n", str);
+            free(str);
+            str = ft_calloc(sizeof(char), 1);
+            if (!str)
+            {
+                ft_printf("Error allocating memory\n");
+                exit (1);
+            }
         }
-        free(strs[0]);
-        free(strs);
+    else
+    {
+        str = ft_realloc(str, len, len + 2);
+        if (!str)
+        {
+            ft_printf("Error reallocating memory\n");
+            exit (1);
+        }
+        str[len] = *c;
+        str[len + 1] = '\0';
     }
 }
 
-static void create_char(char *c, int *pos, int *bit)
-{
-    if (c == NULL)
-        printstrings();
-    strs[0][*pos] = ft_calloc(sizeof(char *), 2);
-    if (strs[0][*pos] == NULL)
-    {
-        ft_printf("Error allocating memory\n");
-        freeall(strs);
-        exit(1);
-    }
-    strs[0][*pos][0] = ft_calloc(sizeof(char), 2);
-    strs[0][*pos][0] = *c;
-    *c = 0;
-    *bit = 0;
-    ++(*pos);
-}
 static void handle_signal(int sig)
 {
     static char c = 0;
     static int bit = 0;
-    static int pos = 0;
 
     if (sig == SIGUSR1)
         c = (c << 1) | 1;
@@ -74,8 +54,10 @@ static void handle_signal(int sig)
         c = (c << 1) | 0;
     ++bit;
     if (bit == 8)
-    {
-        create_char(&c, &pos, &bit);   
+    { 
+        create_char(&c);
+        c = 0;
+        bit = 0;
     }
 }
 
@@ -84,14 +66,13 @@ int main(void)
     pid_t   pid;
 
     pid = getpid();
-    
-    strs = ft_calloc(sizeof(char **), 2);
-    if (!strs)
+    str = ft_calloc(sizeof(char), 1);
+    if (!str)
     {
         ft_printf("Error allocating memory\n"); 
-        return (1);
+        exit (1);
     }
-    ft_printf("%d", pid);
+    ft_printf("%d\n", pid);
     while (1)
     {
         signal(SIGUSR1, handle_signal);
